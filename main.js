a155c2f4f18e877815f8e8ba93337d40b3c22c10
zlwym <zlwymann@gmail.com>
2026-04-07 21:10:00 -0700

    light/dark mode and link to resume

--- main.js
+++ main.js
@@ -3,3 +3,31 @@
 document.addEventListener("DOMContentLoaded", () => {
     console.log("main.js is connected and running!");
 });
+
+const lightRadio = document.getElementById("light");
+const darkRadio = document.getElementById("dark");
+darkRadio.checked = true;
+
+lightRadio.addEventListener("change", function() {
+    if (this.checked) {
+        var element = document.body;
+        var navbar = document.querySelector(".website-nav");
+        
+        element.classList.toggle("body-light");
+        navbar.classList.toggle("website-nav-light");
+
+        darkRadio.checked = false;
+    }
+})
+
+darkRadio.addEventListener("change", function() {
+    if (this.checked) {
+        var element = document.body;
+        var navbar = document.querySelector(".website-nav");
+
+        element.classList.toggle("body-light");
+        navbar.classList.toggle("website-nav-light");
+
+        darkRadio.checked = true;
+    }
+})
